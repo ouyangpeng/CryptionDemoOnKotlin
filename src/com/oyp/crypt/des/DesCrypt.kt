8541ca184b7a2340ebbaf0eb5d5dd921cabba2df
ouyangpeng <oypcz@foxmail.com>
2020-12-22 00:27:08 +0800

    1、完成DES加密解密 2、DES加密解密过程中，使用Base64编码

--- src/com/oyp/crypt/des/DesCrypt.kt
+++ src/com/oyp/crypt/des/DesCrypt.kt
@@ -1,32 +1,35 @@
 package com.oyp.crypt.des
 
+import com.oyp.crypt.encode.Base64Util
 import java.security.Key
 import javax.crypto.Cipher
 import javax.crypto.SecretKeyFactory
 import javax.crypto.spec.DESKeySpec
 
 object DesCrypt {
-    private val CRYPT_METHOD = "DES"
+    //算法/工作模式/填充模式
+    private val TRANSFORMATION = "DES/ECB/PKCS5Padding"
+    private val CRYPT_ALGORITHM = "DES"
 
-    private val cipher = Cipher.getInstance(CRYPT_METHOD)
-    private val desSecretKeyFactory = SecretKeyFactory.getInstance(CRYPT_METHOD)
+    private val cipher = Cipher.getInstance(TRANSFORMATION)
+    private val desSecretKeyFactory = SecretKeyFactory.getInstance(CRYPT_ALGORITHM)
 
-    fun encrypt(originContent: String, desKey: String): ByteArray {
+    fun encrypt(originContent: String, desKey: String): String {
         // 1、初始化DES相关的操作
         val key: Key? = initKey(desKey)
         // 2、初始化cipher对象(参数一：加密模式)
         cipher.init(Cipher.ENCRYPT_MODE, key)
-        // 3、加密
-        return cipher.doFinal(originContent.toByteArray());
+        // 3、Base64编码 加密后的内容
+        return Base64Util.encode(cipher.doFinal(originContent.toByteArray()))
     }
 
-    fun decrypt(encryptArray: ByteArray, desKey: String): ByteArray {
+    fun decrypt(encryptContent: String, desKey: String): String {
         // 1、初始化DES相关的操作
         val key: Key? = initKey(desKey)
         // 2、初始化cipher对象(参数一：解密模式)
         cipher.init(Cipher.DECRYPT_MODE, key)
-        // 3、解密
-        return cipher.doFinal(encryptArray);
+        // 3、Base64解码 解密后的内容
+        return String(cipher.doFinal(Base64Util.decode(encryptContent)))
     }
 
     private fun initKey(desKey: String): Key? {
@@ -44,5 +47,5 @@ fun main(args: Array<String>) {
     println("原文为：【${originContent}】 进行Des加密后的内容为：【${desEncryptContent}】")
 
     val desDecryptContent = DesCrypt.decrypt(desEncryptContent, desKey)
-    println("秘文为：【${desEncryptContent}】 进行Des加密后的内容为：【${String(desDecryptContent)}】")
+    println("秘文为：【${desEncryptContent}】 进行Des解密后的内容为：【${desDecryptContent}】")
 }
