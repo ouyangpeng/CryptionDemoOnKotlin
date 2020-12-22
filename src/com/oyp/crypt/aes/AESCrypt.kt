947c49e989f51bef12840270ff6137f35432bd87
ouyangpeng <oypcz@foxmail.com>
2020-12-22 22:30:45 +0800

    1、AES加密改成CBC工作模式

--- src/com/oyp/crypt/aes/AESCrypt.kt
+++ src/com/oyp/crypt/aes/AESCrypt.kt
@@ -2,34 +2,46 @@ package com.oyp.crypt.aes
 
 import com.oyp.crypt.encode.Base64Util
 import javax.crypto.Cipher
+import javax.crypto.spec.IvParameterSpec
 import javax.crypto.spec.SecretKeySpec
 
 object AESCrypt {
     //AES/CBC/NoPadding (128)
     //AES 秘钥长度是16位，每位8个字节，所以是  16*8=128
 
-    //算法/工作模式/填充模式
-    private val TRANSFORMATION = "AES"
+    //   算法/工作模式/填充模式
+    //   AES/CBC/NoPadding (128)
+    //   AES/CBC/PKCS5Padding(128)
+    //   AES/ECB/NoPadding(128)
+    //   AES/ECB/PKCS5Padding(128)
+    private val TRANSFORMATION = "AES/CBC/PKCS5Padding"
     private val CRYPT_ALGORITHM = "AES"
 
     private val cipher = Cipher.getInstance(TRANSFORMATION)
 
     fun encrypt(originContent: String, aesKey: String): String {
-        // 1、初始化AES相关的操作
-        val key: SecretKeySpec = initKey(aesKey)
-        // 2、初始化cipher对象(参数一：加密模式)
-        cipher.init(Cipher.ENCRYPT_MODE, key)
+        initCipher(aesKey, Cipher.ENCRYPT_MODE)
         // 3、Base64编码 加密后的内容
         return Base64Util.encode(cipher.doFinal(originContent.toByteArray()))
     }
 
     fun decrypt(encryptContent: String, aesKey: String): String {
+        initCipher(aesKey, Cipher.DECRYPT_MODE)
+        // 3、Base64解码 解密后的内容
+        return String(cipher.doFinal(Base64Util.decode(encryptContent)))
+    }
+
+    private fun initCipher(aesKey: String, mode: Int) {
         // 1、初始化AES相关的操作
         val key: SecretKeySpec = initKey(aesKey)
         // 2、初始化cipher对象(参数一：解密模式)
-        cipher.init(Cipher.DECRYPT_MODE, key)
-        // 3、Base64解码 解密后的内容
-        return String(cipher.doFinal(Base64Util.decode(encryptContent)))
+        if (TRANSFORMATION.contains("CBC")) {
+            // CBC工作模式需要额外添加参数,否则报错：java.security.InvalidKeyException: Parameters missing
+            val iv = IvParameterSpec(aesKey.toByteArray())
+            cipher.init(mode, key, iv)
+        } else {
+            cipher.init(mode, key)
+        }
     }
 
     private fun initKey(aesKey: String): SecretKeySpec {
