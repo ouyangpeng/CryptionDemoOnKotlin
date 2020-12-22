5500964ea69dd9a06b4eaf319cc91f2ba23ed10f
ouyangpeng <oypcz@foxmail.com>
2020-12-22 23:41:36 +0800

    1、完成RSA分片加密

--- src/com/oyp/crypt/rsa/RSACrypt.kt
+++ src/com/oyp/crypt/rsa/RSACrypt.kt
@@ -1,29 +1,78 @@
 package com.oyp.crypt.rsa
 
 import com.oyp.crypt.encode.Base64Util
+import java.io.ByteArrayOutputStream
+import java.security.Key
 import java.security.KeyPairGenerator
 import java.security.PrivateKey
 import java.security.PublicKey
 import javax.crypto.Cipher
 
 object RSACrypt {
-    private val TRANSFORMATION = "RSA"
+    /**
+     * 算法
+     */
     val CRYPT_ALGORITHM = "RSA"
+    /**
+     * Cipher对象
+     */
+    val cipher = Cipher.getInstance(CRYPT_ALGORITHM)
 
-    private val cipher = Cipher.getInstance(TRANSFORMATION)
+    /**
+     * 不能大于117个字节，否则会报异常：javax.crypto.IllegalBlockSizeException: Data must not be longer than 117 bytes
+     */
+    val ENCRYPT_MAX_SIZE = 117;
 
+    /**
+     * 使用私钥加密
+     */
     fun encryptByPrivateKey(originContent: String, privateKey: PrivateKey): String {
-        // 初始化cipher
-        cipher.init(Cipher.ENCRYPT_MODE, privateKey)
-        // 3、Base64编码 加密后的内容
-        return Base64Util.encode(cipher.doFinal(originContent.toByteArray()))
+        return segmentedEncrypt(originContent, privateKey)
     }
 
+    /**
+     * 使用公钥加密
+     */
     fun encryptByPublicKey(originContent: String, publicKey: PublicKey): String {
+        return segmentedEncrypt(originContent, publicKey)
+    }
+
+    /**
+     * 分段加密
+     */
+    private fun segmentedEncrypt(originContent: String, key: Key): String {
+
         // 初始化cipher
-        cipher.init(Cipher.ENCRYPT_MODE, publicKey)
+        cipher.init(Cipher.ENCRYPT_MODE, key)
+        // 分段加密
+        val byteArray = originContent.toByteArray()
+        var temp: ByteArray? = null
+        // 当前位置偏移量
+        var offset = 0
+        // 输出流
+        val byteArrayOutputStream = ByteArrayOutputStream()
+        // 如果没有加密完，则一直加密
+        while (byteArray.size - offset > 0) {
+            // 每次加密不能大于117个字节，否则会报异常：javax.crypto.IllegalBlockSizeException: Data must not be longer than 117 bytes
+            if (byteArray.size - offset > ENCRYPT_MAX_SIZE) {
+                // 剩余部分大于117个字节
+                // 加密完整的117个字节
+                temp = cipher.doFinal(byteArray, offset, ENCRYPT_MAX_SIZE)
+                // 重新计算偏移的位置
+                offset += ENCRYPT_MAX_SIZE
+            } else {
+                // 计算最后一块不足117个字节的那块
+                temp = cipher.doFinal(byteArray, offset, byteArray.size - offset)
+                // 重新计算偏移的位置
+                offset = byteArray.size
+            }
+            // 存储到临时缓冲区
+            byteArrayOutputStream.write(temp)
+        }
+        // 关闭输出流
+        byteArrayOutputStream.close()
         // 3、Base64编码 加密后的内容
-        return Base64Util.encode(cipher.doFinal(originContent.toByteArray()))
+        return Base64Util.encode(byteArrayOutputStream.toByteArray())
     }
 }
 
@@ -38,9 +87,11 @@ fun main(args: Array<String>) {
     println("publicKey = ${Base64Util.encode(publicKey.encoded)} ")
     println("privateKey = ${Base64Util.encode(privateKey.encoded)}")
 
-
     // 原文
-    val originContent = "欧阳鹏的博客：http://blog.csdn.net/ouyang_peng"
+    val originContent = """欧阳鹏的博客：http://blog.csdn.net/ouyang_peng 欢迎大家一起来浏览，
+欧阳鹏的博客：http://blog.csdn.net/ouyang_peng欢迎大家一起来浏览，
+欧阳鹏的博客：http://blog.csdn.net/ouyang_peng 欢迎大家一起来浏览,
+欧阳鹏的博客：http://blog.csdn.net/ouyang_peng 欢迎大家一起来浏览"""
 
     // 秘文
     val rsaPrivateKeyEncryptContent = RSACrypt.encryptByPrivateKey(originContent, privateKey)
